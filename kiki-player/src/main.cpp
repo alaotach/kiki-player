3eb313b50a96cbf8de54de992d28b7b6461475d9
alaotach <aryanmi2001@gmail.com>
2026-05-17 15:28:30 +0530

    update main.cpp to include and setup all the components

--- kiki-player/src/main.cpp
+++ kiki-player/src/main.cpp
@@ -3,6 +3,12 @@
 #include <config.h>
 #include <appState.h>
 #include <audio.h>
+#include <display.h>
+#include <rtc.h>
+#include <sdcard.h>
+#include <ui.h>
+#include <input.h>
+#include <battery.h>
 
 AppState appState= {
   Screen::HOME,
@@ -18,18 +24,52 @@ AppState appState= {
   false, // isBluetoothConnected
   50 // volume
 };
+
+static uint32_t lastBatteryCheck = 0;
+static uint32_t lastClockUpdate = 0;
+static uint32_t lastIdle = 0;
+static bool blDim = 0;
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
   audioSetup();
+  Wire.begin(I2C_SCL);
   int result = myFunction(2, 3);
+  displaySplash();
+  batterySetup();
+  rtcSetup();
+  inputSetup();
+  sdCardInit();
+  sdScanTracks();
+  uiSetup();
+  lastIdle = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   audioTick();
+  uint32_t curr = millis();
+  InputEvents event = inputRead();
+  if (event != InputEvents::NONE){
+    uiInputHandler(event);
+    lastIdle = curr;
+  }
+  if (curr-lastBatteryCheck >= 60000){
+    lastBatteryCheck = curr;
+    BatteryStat batt = batteryRead();
+  }
+  uiDraw();
+  uint32_t idle = curr - lastIdle;
+  if (idle >= 60000 && !blDim) {
+    backlightDim();
+    blDim = true;
+  }
+  if (idle < 60000 && blDim) {
+    backlightOn();
+    blDim = false;
+  }
 }
 
 // put function definitions here:
